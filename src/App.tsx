22b4df4716a59c615e0361f906592b70e3f01cb3
Nishan <nishan.bhattrai@aitcentre.com>
2025-04-07 16:44:23 +0545

    fix: build error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
-
 function App() {
   return <RouterProvider router={router} />;
 }
