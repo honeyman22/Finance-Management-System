ebf26ac3f64f22662cc7a22afc0443b94987d024
Nishan <nishan.bhattrai@aitcentre.com>
2025-04-08 11:37:24 +0545

    feat: layout design

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+
+import { useDisclosure } from "@mantine/hooks";
 const Layout = () => {
+  const [open, { toggle }] = useDisclosure(false);
   return (
     <div className="w-full flex justify-between  h-screen">
       <div>
-        <Sidebar />
+        <Sidebar isOpen={open} setOpen={toggle} />
       </div>
       <div className="flex-1  bg-gray-100">
-        <Header />
-        <div className="p-8 h-[calc(100vh-4rem)] overflow-y-auto">
+        <Header setOpen={toggle} />
+        <div className="p-8 h-[calc(100vh-5rem)] overflow-y-auto">
           <Outlet />
         </div>
       </div>
