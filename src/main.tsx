8e2663534742132c84001a8f40316138c06ff177
Nishan <nishan.bhattrai@aitcentre.com>
2025-04-28 14:58:46 +0545

    feat: pending deposit and loans application in dashboard added

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "@mantine/dates/styles.css";
 import "@mantine/dropzone/styles.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastContainer } from "react-toastify";
+import { Bounce, ToastContainer } from "react-toastify";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
 const queryClient = new QueryClient({
@@ -27,5 +27,18 @@ createRoot(document.getElementById("root")!).render(
       </MantineProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Bounce}
+    />
   </StrictMode>
 );
